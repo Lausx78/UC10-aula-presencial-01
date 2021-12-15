ae70ab763611d931e3cb46d2e5c85fe405ab27a6
Lausx78 <lausx78@gmail.com>
2021-12-15 14:34:55 -0300

    UC10-aula-presencial-01

---
+++ src/app/components/cadastro/cadastro-delete/cadastro-delete.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Cadastro } from '../cadastro.model';
+import { CadastroService } from '../cadastro.service';
+
+@Component({
+  selector: 'app-cadastro-delete',
+  templateUrl: './cadastro-delete.component.html',
+  styleUrls: ['./cadastro-delete.component.css']
+})
+export class CadastroDeleteComponent implements OnInit {
+  cadastro!: Cadastro;
+
+  constructor(private cadastroService:CadastroService, private router:Router, private route:ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = +Number (this.route.snapshot.paramMap.get('id'))
+  this.cadastroService.readById(id).subscribe((cadastro) => {
+    this.cadastro = cadastro;
+  })
+  }
+  deleteCadastro():void{
+    this.cadastroService.deleteCadastro (Number (this.cadastro.id)).subscribe(() =>{
+      this.cadastroService.showMessege('Cadastro deletado')
+    })
+    
+  }
+  
+cancelarCadastro(): void{
+  this.router.navigate(['/cadastro'])
+  
+  }
+  
+  tabelasCadastro(): void{
+    this.router.navigate(['/cadastro/tabela'])
+  
+  
+  }
+}
